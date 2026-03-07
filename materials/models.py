ba67547031b52f9db5eef1757fc00a0d973e06d1
Eugene Zorin <militarydriverb@gmail.com>
2026-03-07 16:40:09 +0300

    Homework 32.1: Implementation

--- materials/models.py
+++ materials/models.py
@@ -72,8 +72,20 @@ class Lesson(models.Model):
     )
 
     class Meta:
-        verbose_name = "Курс"
-        verbose_name_plural = "Курсы"
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
 
     def __str__(self):
         return self.name
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь')
+    course = models.ForeignKey('Course', on_delete=models.CASCADE, verbose_name='Курс')
+
+    def __str__(self):
+        return f"{self.user} - {self.course}"
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
