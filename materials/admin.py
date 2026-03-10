5e78f52b25552d5758cfe4ac6f8f9bfc48890eb7
Eugene Zorin <militarydriverb@gmail.com>
2026-03-10 22:23:34 +0300

    Revert "feature/Homework_32.2"

--- materials/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from materials.models import Course, Lesson, Subscription
-
-admin.site.register(Course)
-admin.site.register(Lesson)
-admin.site.register(Subscription)
